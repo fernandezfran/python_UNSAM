b57a5a0ed9a17f76d0d4ae471c719b815d5f8451
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2021-05-20 11:15:25 -0300

    Clase10/ .2 protocolo de iteración

---
+++ ejercicios/Clase10/lote.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" lote.py
+Lote de cajones de una misma fruta
+"""
+
+class Lote:
+    def __init__(self, nombre, cajones, precio):
+        self.nombre  = nombre
+        self.cajones = cajones
+        self.precio  = precio
+
+    def costo(self):
+        return self.cajones * self.precio
+
+    def vender(self, n):
+        self.cajones -= n
+
+    def __repr__(self):
+        return f'Lote({self.nombre}, {self.cajones}, {self.precio})'
+
+
+class MiLote(Lote):
+    def __init__(self, nombre, cajones, precio, factor):
+        super().__init__(nombre, cajones, precio)
+        self.factor = factor
+
+    def rematar(self):
+        self.vender(self.cajones)
+
+    def costo(self):
+        return self.factor * self.cajones * self.precio
+
+    def precio(self):
+        costo_orig = super().costo()
+        return self.factor * costo_orig
