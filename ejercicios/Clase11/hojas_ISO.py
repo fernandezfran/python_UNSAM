6b24987cba88c8b0ff41f1df2f492328bca4a292
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2021-05-27 15:25:57 -0300

    Clase11/ hojas ISO

---
+++ ejercicios/Clase11/hojas_ISO.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ISO 216 : tamaños de papel
+"""
+
+def A(N):
+    """
+    para un valor de N > 0 devuelve el ancho y el largo de la hoja A{N} en mm
+    """
+    if N == 0:
+        return 841, 1189
+    else:
+        ancho, largo = A(N-1)
+        return largo // 2, ancho
+
+
+if __name__ == '__main__':
+    for i in range(6):
+        ancho, largo = A(i)
+        print(f'La hoja A{i} mide ({ancho} x {largo} mm)')
