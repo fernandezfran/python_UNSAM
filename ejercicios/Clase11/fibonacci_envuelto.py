fd7c1ecb54025af31711c4b3adfa029b162bb27b
Francisco Fernandez <fernandezfrancisco2195@gmail.com>
2021-05-27 15:09:37 -0300

    Clase11/ fibonacci

---
+++ ejercicios/Clase11/fibonacci_envuelto.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+fibonacci envuelto
+"""
+
+def fibonacci(n):
+    """
+    Toma un entero positivo n y
+    devuelve el n-ésimo número de Fibonacci
+    donde F(0) = 0 y F(1) = 1.
+    """
+    def fibonacci_aux(n, dict_fibo):
+        """
+        Calcula el n-ésimo número de Fibonacci de forma recursiva
+        utilizando un diccionario para almacenar los valores ya computados.
+        dict_fibo es un diccionario que guarda en la clave 'k' el valor de F(k)
+        """
+        if n in dict_fibo.keys():
+            F = dict_fibo[n]
+        else:
+            F1, dict_fibo = fibonacci_aux(n-1, dict_fibo)
+            F2, dict_fibo = fibonacci_aux(n-2, dict_fibo)
+            F = F1 + F2
+            dict_fibo[n] = F
+        return F, dict_fibo
+    
+    dict_fibo = {0:0, 1:1} 
+    F, dict_fibo = fibonacci_aux(n, dict_fibo)
+    return F 
